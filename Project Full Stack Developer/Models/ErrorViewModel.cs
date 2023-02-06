037b5e302365f7c576f4adba6d3d1ddc9fd91c79
Oliveirathomas <oliveirathomas@gmail.com>
2023-02-06 19:46:52 +0000

    Add project files.

---
+++ Project Full Stack Developer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_Full_Stack_Developer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
